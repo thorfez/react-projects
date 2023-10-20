6d97191ca24d9b273973aae15b6b0594345f6743
thorfez <125116354+thorfez@users.noreply.github.com>
2023-10-20 06:33:47 +0330

    Add files via upload

---
+++ first-project-codes/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './componnets/navbar'
+import Main from './componnets/main'
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Main />
+    </>
+  )
+}
+
+export default App
